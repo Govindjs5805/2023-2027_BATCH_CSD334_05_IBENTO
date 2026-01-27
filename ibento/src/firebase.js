445a3bd8384a794b75a328234b5fdfa0b329e2f1
Govind J S <govindjs005@gmail.com>
2026-01-27 16:07:00 +0530

    edited firebase

---
+++ ibento/src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOyTO0Qzcw8oJ0n1FK4q8-3PUoaa9LzMY",
+    authDomain: "ibento-campus-event-management.firebaseapp.com",
+    projectId: "ibento-campus-event-management",
+    storageBucket: "ibento-campus-event-management.firebasestorage.app",
+    messagingSenderId: "660567504207",
+    appId: "1:660567504207:web:b2f69b6c25c978785f813a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
