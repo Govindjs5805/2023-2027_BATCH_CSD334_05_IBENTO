abf91e9399dec86d1a3e2bdd4001eb5c25fc0ae4
Govind J S <govindjs005@gmail.com>
2026-02-23 14:17:43 +0530

    edited

--- ibento/src/components/ProtectedRoute.jsx
+++ ibento/src/components/ProtectedRoute.jsx
@@ -2,14 +2,18 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 function ProtectedRoute({ children, allowedRole }) {
-  const { user, role } = useAuth();
+  const { user, role ,loading } = useAuth();
 
-  if (!user) return <Navigate to="/login" />;
+  if (loading) return null;
 
-  if (allowedRole && role !== allowedRole)
+  if(!user){
+    return <Navigate to="/login" />;
+  }
+  if (allowedRole && user.role !== allowedRole) {
     return <Navigate to="/" />;
+  }
 
   return children;
-}
 
+}
 export default ProtectedRoute;
